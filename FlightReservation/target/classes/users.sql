afd885bd02819e52520cb0b35a1d48bb83b0c862
Arya-Yellapu <111416089+Arya-Yellapu@users.noreply.github.com>
2022-10-03 21:15:22 +0530

    commit

---
+++ FlightReservation/target/classes/users.sql
@@ -0,0 +1,9 @@
+create table users(
+customername varchar(35) primary key,
+customerpassword varchar(35),
+firstname varchar(35),
+lastname varchar(35),
+dob date,
+email varchar(35));
+
+insert into users values('Walter','White','walter','white','1997-03-10','walter@ymail.com');
