b358a2b4023a992d2e71d2f9c73a4366a35ecfe6
Arya-Yellapu <111416089+Arya-Yellapu@users.noreply.github.com>
2022-10-05 00:00:16 +0530

    Delete Flights directory

--- Flights/Flights/src/main/java/com/project/Service/FlightService.java
+++
@@ -1,9 +0,0 @@
-package com.project.Service;
-
-import java.util.List;
-
-import com.project.Model.Flights;
-
-public interface FlightService {
-	public List<Flights> getFlightsAvailable(String source, String destination, String traveldate) throws Exception;
-}
--- Flights/src/main/resources/seats.sql
+++
@@ -1,8 +0,0 @@
-create table seats(
-seatno varchar(35),
-status varchar(35),
-reservationid varchar(35),
-flightno varchar(35),
-constraint foreign_key foreign key(flightno) REFERENCES flights(flightno));
-
-insert into seats(seatno,status,flightno) values('E01','Available','T1001'),('E02','Available','T1001'),('E03','Available','T1001'),('E04','Available','T1001'),('E05','Available','T1001');
