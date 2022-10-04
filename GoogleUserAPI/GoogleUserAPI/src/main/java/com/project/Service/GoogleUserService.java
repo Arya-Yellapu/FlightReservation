45b8180a80caaf9e3337fd410e8e490911a3a4d8
Arya-Yellapu <111416089+Arya-Yellapu@users.noreply.github.com>
2022-10-05 00:00:30 +0530

    Delete GoogleUserAPI directory

--- GoogleUserAPI/GoogleUserAPI/src/main/java/com/project/Service/GoogleUserService.java
+++
@@ -1,9 +0,0 @@
-package com.project.Service;
-
-import com.project.Model.GoogleUser;
-
-public interface GoogleUserService {
-
-	public GoogleUser getData(String customername, String customerpassword) throws Exception;
-	
-}
--- GoogleUserAPI/src/main/java/com/project/DAO/GoogleUserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.project.DAO;
-
-import com.project.Model.GoogleUser;
-
-public interface GoogleUserDAO {
-
-public GoogleUser getData(String customername, String customerpassword);	
-	
-}
