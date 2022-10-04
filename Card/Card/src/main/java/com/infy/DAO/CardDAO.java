2d37946995f241e94b54f0dcbca7b383dec43232
Arya-Yellapu <111416089+Arya-Yellapu@users.noreply.github.com>
2022-10-04 23:59:51 +0530

    Delete Card directory

--- Card/Card/src/main/java/com/infy/DAO/CardDAO.java
+++
@@ -1,7 +0,0 @@
-package com.infy.DAO;
-
-public interface CardDAO {
-
-public int checkValidity(long cardNo, int cvv, int expiryMonth, int expiryYear, String name);	
-	
-}
--- Card/src/main/resources/account.sql
+++
@@ -1,7 +0,0 @@
-create table account(
-accountno bigint primary key,
-accountholdername varchar(35),
-accountbalance bigint,
-accounttype varchar(35));
-
-insert into account values(56785678,'Arya',1000000,'Savings');
